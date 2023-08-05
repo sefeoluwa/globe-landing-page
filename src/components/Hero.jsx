9b5822f23825b7f083863a80d16c1489c612f025
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 23:45:38 +0100

    styles for button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ function Hero() {
         <h1 className='text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
         <p className='pl-[2.5vw] text-[#696871] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
 
-        <div className="start pl-[2.5vw] mt-10">
-          <button>Get Started</button>
+        <div className="start pl-[2.5vw] mt-10 flex">
+          <button className='w-[211px] h-[50px] bg-[#5454D4] text-white rounded-xl text-[17px]'>Get Started</button>
           <FaArrowDown className='rounded-full border-solid border-grey border-2'/>
         </div>
 
