20ae9ce612eb5b90beb5338ac75b1dbe1f345949
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 17:06:51 +0100

    added all images

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import earth_dark from "./earth_dark.png"
+import earth_light from "./earth_light.png"
+import icon1 from "./icon1.png"
+import icon2 from "./icon2.png"
+import icon3 from "./icon3.png"
+import logo1 from "./logo1.svg"
+import logo2 from "./logo2.svg"
+import logo3 from "./logo3.svg"
+import logo4 from "./logo4.svg"
+import logo5 from "./logo5.svg"
+import logo from "./logo.svg"
+
+export {
+    earth_dark,
+    earth_light,
+    icon1,
+    icon2,
+    icon3,
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo,
+}
