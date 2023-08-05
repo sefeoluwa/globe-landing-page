cd6916214cd8d188f1e3be8cf454f2140a5b9cf0
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 23:41:01 +0100

    font and margin for paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ import { FaArrowDown } from "react-icons/fa";
 function Hero() {
   return (
     <>
-   <div className="hero">
+   <div className="hero mt-20">
       <div className="hero-left w-[459px] h-[461px]">
 
         <h1 className='text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
-        <p className='pl-[2.5vw]'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
+        <p className='pl-[2.5vw] text-[#696871] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
 
-        <div className="start pl-[2.5vw]">
+        <div className="start pl-[2.5vw] mt-10">
           <button>Get Started</button>
           <FaArrowDown className='rounded-full border-solid border-grey border-2'/>
         </div>
