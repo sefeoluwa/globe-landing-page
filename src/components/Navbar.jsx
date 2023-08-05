b846a90cb5e5d4fe6e2499f4b2cd6f1347d7068c
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 19:27:07 +0100

    padding top for li

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import { logo } from "../assets"
 function Navbar() {
   return (
    <>
-   <nav className="flex justify-center pt-5" >
-    <div className="nav-container flex justify-between w-[70%]">
+   <nav className="flex justify-center pt-5 " >
+    <div className="nav-container flex justify-between w-[75%]">
     <div className="">
       <img src={logo} alt="Logo for page" className=""/>
     </div>
 
-    <ul className="flex justify-around w-[8.6cm]">
+    <ul className="flex justify-around w-[9cm] pt-2 text-[15px]">
     <li>Home</li>
     <li>Features</li>
     <li>Pricing</li>
     <li>Blog</li>
     </ul>
 
-    <button className="ml-[200px]">Get Started</button>
+    <button className="">Get Started</button>
     </div>
    </nav>
    </>
