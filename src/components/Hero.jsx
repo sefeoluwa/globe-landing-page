62eaee9f02c01deacd9e3c47234c283d0bb57590
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 23:14:15 +0100

    content for left side

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { SectionWrapper } from "../hoc"
+import { FaArrowDown } from "react-icons/fa";
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <>
+   <div className="hero">
+      <div className="hero-left">
+
+        <h1>Grow your subscription business</h1>
+        <p>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
+
+        <div className="start">
+          <button>Get Started</button>
+          <FaArrowDown className='rounded-full border-solid border-grey border-2'/>
+        </div>
+
+      </div>
+   </div>
+    </>
   )
 }
 
-export default Hero
+export default SectionWrapper(Hero, 'hero')
