ed20a221ec8278b1cf67fc38beb11ef42d75f507
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-05 23:25:50 +0100

    font and sizing for heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
   return (
     <>
    <div className="hero">
-      <div className="hero-left">
+      <div className="hero-left w-[459px] h-[461px]">
 
-        <h1>Grow your subscription business</h1>
-        <p>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
+        <h1 className='text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
+        <p className='pl-[2.5vw]'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
 
-        <div className="start">
+        <div className="start pl-[2.5vw]">
           <button>Get Started</button>
           <FaArrowDown className='rounded-full border-solid border-grey border-2'/>
         </div>
