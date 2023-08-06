968d3e8481294e5177ffa12773a42b3bfdd38aa6
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-07 00:00:39 +0100

    rendered content and image

--- src/components/SubIndex.jsx
+++ src/components/SubIndex.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/no-unescaped-entities */
+import { earth_dark } from "../assets"
+import { SectionWrapper } from "../hoc"
 
 function SubIndex() {
   return (
-    <div>SubIndex</div>
+    <div className="flex">
+      <img src={earth_dark} alt= "Dark colored globe image" />
+
+      <div className="">
+        <h2>Subscription Index</h2>
+        <p>A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.</p>
+        <p>Learn more</p>
+      </div>
+    </div>
   )
 }
 
-export default SubIndex
+export default SectionWrapper(SubIndex, '')
