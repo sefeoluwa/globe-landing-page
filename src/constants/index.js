d1e70ced17af7cb684d58feda845114f18589d12
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-06 23:30:56 +0100

    added logo icons

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+import { 
+    logo1, 
+    logo2, 
+    logo3,
+    logo4,
+    logo5, 
+} from "../assets"
+
+const Logos = [
+    {
+        name: "logo1",
+        icon: logo1,
+      },
+      {
+        name: "logo2",
+        icon: logo2,
+      },
+      {
+        name: "logo3",
+        icon: logo3,
+      },
+         {
+        name: "logo4",
+        icon: logo4,
+      },
+      {
+        name: "logo5",
+        icon: logo5,
+      },
+]
+
+export default Logos
