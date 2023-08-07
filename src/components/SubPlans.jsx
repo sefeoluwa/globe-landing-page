a6c8bcd8a67b5d999cc03746256c1252cef82b2a
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-07 14:59:28 +0100

    rendered text content without styles

--- src/components/SubPlans.jsx
+++ src/components/SubPlans.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import { SectionWrapper } from "../hoc"
+import { plans } from "../constants"
 
 function SubPlans() {
   return (
-    <div>SubPlans</div>
+    <>
+    <div className="top flex justify-between">
+      <h2>Get the right plan for future product.</h2>
+      <div className="buttons">
+        <button>Yearly</button>
+        <button>Monthly</button>
+      </div>
+    </div>
+
+    <div className="bottom">
+      <div className='flex justify-evenly'>
+        {plans.map((plan) => (
+          <div key={plan.title}>
+            <p>{plan.title}</p>
+            <h3>{plan.price}</h3>
+            <ul className='flex flex-col'>
+              <li className=''>{plan.specs}</li>
+            </ul>
+            <button>{plan.button}</button>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
   )
 }
 
