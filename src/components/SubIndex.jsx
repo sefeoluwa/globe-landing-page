4e0f648f581835f693a392671f2f92a39547f4c7
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-07 10:23:22 +0100

    font styles for text content

--- src/components/SubIndex.jsx
+++ src/components/SubIndex.jsx
@@ -9,9 +9,9 @@ function SubIndex() {
       <img src={earth_dark} alt= "Dark colored globe image" />
 
       <div className="">
-        <h2>Subscription Index</h2>
-        <p>A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.</p>
-        <p>Learn more</p>
+        <h2 className="text-[50px] font-bold leading-[57px] tracking-[-1.56px]">Subscription Index</h2>
+        <p className=" text-[20px] leading-[30px] text-[#696871] tracking-[-0.63px] font-['Mulish']">A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.</p>
+        <p className="font-bold text-[17px] text-[#5454d4] cursor-pointer pt-[30px]">Learn more</p>
       </div>
     </div>
   )
