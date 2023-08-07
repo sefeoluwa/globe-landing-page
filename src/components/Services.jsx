bc7ca07cb29c8f24e30b44b905b52c2f2266debb
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-07 14:15:22 +0100

    added padding to paragraph

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,7 +10,7 @@ function Services() {
         <div className="w-[60vw]  " key={service.name}>
           <img src={service.icon} alt=""/>
           <h1 className=' text-center font-bold text-[24px] leading-[36px] tracking-[-0.75px] pr-[4vw]'>{service.header}</h1>
-          <p className='w-[307px] text-center text-[16px] leading-[24px] text-[#696871] tracking-[-0.5px] font-[Mulish] '>{service.para}</p>
+          <p className='w-[307px] text-center text-[16px] leading-[24px] text-[#696871] tracking-[-0.5px] font-[Mulish] mt-[20px]'>{service.para}</p>
         </div>
 ))}
     </div>
