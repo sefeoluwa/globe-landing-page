875d7cac4d99c2aa28a15ee2d57b9f1bc7574e72
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-08 14:00:45 +0100

    animated side bar render on mobiles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { logo, menu, close } from "../assets";
-import { motion, useAnimation } from "framer-motion";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { sideVariants, itemVariants } from "/home/user/repos/globe-landing-page/utils/motion.js"
+
 
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -14,6 +16,9 @@ function Navbar() {
   const handleMobileMenuToggle = () => {
     setIsMobileMenuOpen((prevState) => !prevState);
   };
+
+  // animate sidebar render on mobiles
+
 
   return (
     <>
@@ -44,8 +49,19 @@ function Navbar() {
 
 
       {/* Mobile Navbar */}
-      <nav className="md:hidden flex justify-center pt-5 fixed z-10 w-[90vw] ">
-        <div className="nav-container flex justify-between w-[75%]">
+      
+    <AnimatePresence>
+      <motion.aside 
+       initial={{ width: 0 }}
+       animate={{
+         width: 500
+       }}
+       exit={{
+         width: 0,
+         transition: { delay: 0.7, duration: 0.3 }
+       }}
+      className="md:hidden flex justify-center pt-5 fixed z-10 w-[90vw] ">
+        <motion.div className="nav-container flex justify-between w-[75%]">
           <div className="">
             <img src={logo} alt="Logo for page" className="" />
           </div>
@@ -61,17 +77,24 @@ function Navbar() {
               <img src={menu} alt="Menu" />
             )}
           </button>
-        </div>
-      </nav>
+        </motion.div>
+
+
+      </motion.aside>
 
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
-        <div className="md:hidden flex flex-col fixed bg-[#5454D4] text-white w-[55vw] ml-[35vw] mt-[10vh] h-[350px] items-end pr-[18vw] z-10 rounded-2xl pt-4">
+        <motion.div 
+        initial="closed"
+        animate="open"
+        exit="closed"
+        variants={sideVariants}
+        className="container md:hidden flex flex-col fixed bg-[#5454D4] text-white w-[55vw] ml-[35vw] mt-[10vh] h-[350px] items-end pr-[18vw] z-10 rounded-2xl pt-4">
           <ul className="text-[20px] mr-[-25px] p-5">
-            <li className="mb-6">Home</li>
-            <li className="mb-6">Features</li>
-            <li className="mb-6">Pricing</li>
-            <li className="mb-6">Blog</li>
+            <motion.li variants={itemVariants} className="mb-6">Home</motion.li>
+            <motion.li variants={itemVariants} className="mb-6">Features</motion.li>
+            <motion.li variants={itemVariants} className="mb-6">Pricing</motion.li>
+            <motion.li variants={itemVariants} className="mb-6">Blog</motion.li>
           </ul>
           <motion.button
             onHoverStart={handleHover}
@@ -81,8 +104,9 @@ function Navbar() {
           >
             Get Started
           </motion.button>
-        </div>
+        </motion.div>
       )}
+</AnimatePresence>
     </>
   );
 }
