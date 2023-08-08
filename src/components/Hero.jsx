5ee3cd68d43a3efc8248b7beb4022509c0f9aefb
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-08 18:29:48 +0100

    added classes for styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,11 @@ import { downArrow, earth_light }from "../assets"
 function Hero() {
   return (
     <>
-   <section className="hero mt-20 flex w-[80vw] pl-[3vw]">
+   <section className="hero mt-20 flex w-[80vw] pl-[3vw] overflow-hidden">
       <div className="hero-left w-[459px] h-[461px] mt-5 ml-[-4vw]">
 
-        <h1 className='text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
-        <p className='pl-[2.5vw] text-[#696871] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
+        <h1 className='hero-head text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
+        <p className=' hero-p pl-[2.5vw] text-[#696871] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
 
         <div className="start pl-[2.5vw] mt-10 flex">
           <button className='w-[211px] h-[50px] bg-[#5454D4] text-white rounded-xl text-[17px]'>Get Started</button>
@@ -22,7 +22,7 @@ function Hero() {
 
       </div>
       <div className="light-earth  w-[70vw] overflow-hidden ml-[-4vw]">
-      <img src={earth_light} alt="" className='w-[70vw] h-[70vh] mt-[-12vh]'/>
+      <img src={earth_light} alt="" className='w-[70vw]  h-[70vh] mt-[-12vh]'/>
       </div>
      
    </section>
