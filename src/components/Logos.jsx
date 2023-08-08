d066e514f79425d0ff21c3f62027220fbd48c597
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-09 00:27:47 +0100

    animated render with aos

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react-refresh/only-export-components */
+import { useEffect } from "react"
 import { Icons } from "../constants"
 import { SectionWrapper } from "../hoc"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 function Logos() {
+  useEffect (() => {
+    AOS.init({duration: 1500});
+}, []);
   return (
     <>
-    <div className="logos flex flex-wrap gap-10 justify-between">
+    <div data-aos="fade-right" className="logos flex flex-wrap gap-10 justify-between mt-5">
     {Icons.map((icons) => (
       <img src={icons.icon} key={icons.name} alt="" />
     ))}
