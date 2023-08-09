bbe30190aaf848df6928f8f1a25dbf85957f74c3
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-09 09:30:48 +0100

    motion and AOS animations

--- src/components/SubIndex.jsx
+++ src/components/SubIndex.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/no-unescaped-entities */
+import { useEffect } from "react"
 import { earth_dark } from "../assets"
 import { SectionWrapper } from "../hoc"
+import { zoomIn } from "../../utils/motion"
+import { motion } from "framer-motion"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 function SubIndex() {
+
+  useEffect (() => {
+    AOS.init({duration: 1500});
+}, []);
+
   return (
     <div className="subIndex flex justify-center items-center w-full ml-[-4vw]">
-      <img src={earth_dark} alt= "" />
+      <motion.img 
+      variants={zoomIn('0.2', '1' )} 
+      src={earth_dark} 
+      alt= "" />
 
-      <div className="index-container">
+      <div data-aos="flip-up"
+       className="index-container">
         <h2 className="text-[50px] font-bold leading-[57px] tracking-[-1.56px] mb-[30px] w-[14.7cm]">Subscription Index</h2>
         <p className=" text-[20px] leading-[30px] text-[#696871] tracking-[-0.63px] font-['Mulish'] w-[82%]">A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.</p>
         <p className="font-bold text-[17px] text-[#5454d4] cursor-pointer pt-[30px]">Learn more</p>
