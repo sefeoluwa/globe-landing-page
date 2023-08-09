e28c9ac76f0fd825ca548a81c945ffa6a1b06315
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-09 09:43:33 +0100

    AOS animation

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable react-refresh/only-export-components */
-import React from 'react'
+import React, {useEffect} from 'react'
 import { services } from "../constants"
 import { SectionWrapper } from "../hoc"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 function Services() {
+
+  useEffect (() => {
+    AOS.init({duration: 1500});
+}, []);
+
   return (
     <div className='flex justify-start items-end h-[50vh] services '>
       {services.map((service) => (
-        <div className="w-[60vw]  " key={service.name}>
+        <div 
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+        className="w-[60vw] h-auto" 
+        key={service.name}
+        >
           <img src={service.icon} alt=""/>
           <h1 className=' text-center font-bold text-[24px] leading-[36px] tracking-[-0.75px] pr-[4vw]'>{service.header}</h1>
           <p className='w-[307px] text-center text-[16px] leading-[24px] text-[#696871] tracking-[-0.5px] font-[Mulish] mt-[20px]'>{service.para}</p>
